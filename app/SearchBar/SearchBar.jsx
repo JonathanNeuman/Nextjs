f14a4c2d0e957f195ab4bb3156feb675ded718e1
Jonathan Neuman <jonrileyneuman@gmail.com>
2023-11-03 15:28:14 -0600

    Added search bar component

--- app/SearchBar/SearchBar.jsx
+++ app/SearchBar/SearchBar.jsx
@@ -1,8 +1,30 @@
 import { useEffect,useState } from "react"
+import "./SearchBar.css"
+
+
+
 export default function SearchBar(){
-    return
-    <div>
-    
+    const [filteredProducts,setFilteredProducts] = useState([])
+    function fetchAPIData(){
+        fetch('https://fakestoreapi.com/products')
+        .then(response=>response.json())
+        //.then(result=>console.log(result))
+        .then(products => {
+            const currentFilter = products.map((product, index) =>{
+                return (<div className="productStyle">
+                        <p>{product.title}</p>
+                        <p>${product.price}</p>
+                        <img style= {{width: "100px"}} src={product.image} alt={product.title}/>
+                    </div>)
+            })
+            setFilteredProducts(currentFilter)
+        })
+    }
+    useEffect(fetchAPIData, []) // on page load, call fetchAPIData
+
+    return <div>
+        <p>The Search Bar</p>
+        <div style={{display:"flex", flexFlow:"row wrap"}}>{filteredProducts}</div>
     </div>
     
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 import CountButton from "./CountButton/CountButton"
+import SearchBar from "./SearchBar/SearchBar"
 export default function Page() {
     return<><CountButton incrementBy={1}/><hr/>
     <CountButton incrementBy={2}/>
     <a href="https://github.com/JonathanNeuman/Nextjs">My GitHub Repo</a>
+    <SearchBar/>
     </> 
     
   }
